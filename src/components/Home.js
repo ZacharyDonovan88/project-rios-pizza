8c3492aa1ba5cf6d7ac9251fd7a9ebbcaa161d58
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-07-11 17:37:33 +0930

    Co-authored-by: Alex Waite <Alex-Waite@users.noreply.github.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,19 +20,23 @@ function Home() {
           <div className="homeTextImg-container">
             <div className="homeImg pizza-image">
               <div className="homeText">
-                <h3>Come get your Fresh Pizza!</h3>
+                        <h3>Come get your Fresh Pizza!</h3>
               </div>
-              <div className="pizzaSteam-container">
-                <div className="homePizzaImg-container">
-                  <div className="homePizzaImg">
-                    <img src={require('./assets/images/homeImg.png')} alt="Fresh Pizza"/>
+                <div className="pizzaSteam-container">
+                  <div className="homeBanner-container">
+                    <div>
+                      <div className="homePizzaImg-container">
+                        <div className="homePizzaImg">
+                          <img src={require('./assets/images/homeImg.png')} alt="Fresh Pizza"/>
+                        </div>
+                          <div>
+                          {/* <img class="steam-animation" src={require('./assets/images/steam.png')} alt="Steam" /> */}
+                          <img class="steam-animation" src={require('./assets/images/smoke.png')} alt="Steam" />
+                        </div>
+                      </div> 
+                    </div>
                   </div>
                 </div>
-                  <div>
-                    {/* <img class="steam-animation" src={require('./assets/images/steam.png')} alt="Steam" /> */}
-                    <img class="steam-animation" src={require('./assets/images/smoke.png')} alt="Steam" />
-                  </div>
-              </div>
             </div>
           </div>
         </div>
